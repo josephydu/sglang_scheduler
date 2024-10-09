0a67eed90ac662afae6654b1f71dfec8f69ed402
YouYangxiu <1824665362@qq.com>
2024-10-09 14:32:27 +0800

    test register

--- controller.py
+++ controller.py
@@ -74,7 +74,7 @@ class Controller:
                 target_node = self.node_list[self.round_robin_counter]
                 self.round_robin_counter = (self.round_robin_counter + 1) % len(self.node_list)
                 async with session.post(
-                    url=f'http://{target_node["ip"]}:{target_node["port"]}/{base_url}',
+                    url=f'http://{target_node.ip}:{target_node.port}/{base_url}',
                     json=pay_load) as res:
                     if res.status == 200:
                         async def stream_response():
