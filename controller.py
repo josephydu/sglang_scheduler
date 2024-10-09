10539e298bfceeb641ae2796678184e17d0fa11d
YouYangxiu <1824665362@qq.com>
2024-10-09 12:53:45 +0800

    test register

--- controller.py
+++ controller.py
@@ -70,6 +70,7 @@ class Controller:
                 target_node = self.node_list[self.round_robin_counter]
                 self.round_robin_counter = (self.round_robin_counter + 1) % len(self.node_list)
                 json_data = await req.json()
+                print(json_data)
                 async with session.post(
                     url=f'http://{target_node.ip}:{target_node.port}/{base_url}',
                     json=json_data) as res:
