c94724f6c2d6c6285d131da54dafaffc05fd5506
YouYangxiu <1824665362@qq.com>
2024-10-09 13:22:32 +0800

    test register

--- controller.py
+++ controller.py
@@ -73,6 +73,7 @@ class Controller:
                 target_node = self.node_list[self.round_robin_counter]
                 self.round_robin_counter = (self.round_robin_counter + 1) % len(self.node_list)
                 pay_load = await req.json()
+                print(f'[controller]{pay_load}')
                 async with session.post(
                     url=f'http://{target_node.ip}:{target_node.port}/{base_url}',
                     json=pay_load) as res:
