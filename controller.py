9102e3d3b3c3bcdfb6ae3e83014879d6b691a425
YouYangxiu <1824665362@qq.com>
2024-10-10 13:45:04 +0800

    pwoer of 2 choice

--- controller.py
+++ controller.py
@@ -11,6 +11,7 @@ import aiohttp
 import logging
 
 import threading
+import random
 logger = logging.getLogger(__name__)
 AIOHTTP_TIMEOUT = aiohttp.ClientTimeout(total=6 * 60 * 60)
 
@@ -43,6 +44,7 @@ class Controller:
 
         self.node_list = []
         self.recv_controller_procs = []
+        self.controller_info_dict = {}# key is "ip:port", value is "(available memory, running, waiting)"
         
         
         self.round_robin_counter = 0
@@ -70,13 +72,18 @@ class Controller:
     def recv_controller_info_loop(self, recv_controller_info):
         while True:
             try:
-                controller_info = recv_controller_info.recv_string(zmq.NOBLOCK)
-                logger.info(controller_info)
+                recv_controller_info = recv_controller_info.recv_string(zmq.NOBLOCK)
+                # logger.info(controller_info)
+                if recv_controller_info is not None and recv_controller_info != "":
+                    ip, port, avaliable_memory, num_running, num_waitting = recv_controller_info.split(",")
+                    self.controller_info_dict[f'{ip}:{port}'] = (avaliable_memory, num_running, num_waitting)
             except zmq.Again:
                 pass
         
-    # TODO change it to send requests to nodes.
+    #change it to send requests to nodes.
     async def round_robin_scheduler(self, input_requests, base_url):
+        if self.input_requests == 0 or self.node_list == 0:
+            return 
         async with aiohttp.ClientSession(timeout=AIOHTTP_TIMEOUT) as session:
             for req in input_requests:
                 pay_load = req.dict()
@@ -92,7 +99,42 @@ class Controller:
                         print("Failed to retrieve data:", response.status)
                         yield b''  # 返回空字节，表示错误或无数据
 
+    async def power_of_2_choice(self, input_requests, base_url):
+        if self.input_requests == 0 or self.node_list == 0 or len(self.controller_info_dict) == 0:
+            return 
+        async with aiohttp.ClientSession(timeout=AIOHTTP_TIMEOUT) as session:
+            for req in input_requests:
+                # power of 2 choice 
+                if len(self.controller_info_dict) == 1:
+                    target_node, infos = next(iter(self.controller_info_dict.items()))
+                else:
+                    # random choose 2 nodes
+                    (k1, v1), (k2, v2) = random.sample(list(self.controller_info_dict.items()), 2)
+                    
+                    # 1. compare the waiting
+                    if v1[2] != v2[2]:
+                        target_node = k1 if v1[2] < v2[2] else k2
+                    elif v1[1] != v2[1]:
+                        target_node = k1 if v1[1] < v2[1] else k2
+                    elif v1[0] != v2[0]:
+                        target_node = k1 if v1[0] > v2[0] else k2
+                    else:
+                        target_node = k1
+                logger.info(f"choosen nodes in power of 2 choice is [{target_node}]")
+                url = f'http://{target_node}/{base_url}'
+                
+                
+                pay_load = req.dict()
+                async with session.post(url, json=pay_load) as response:
+                    if response.status == 200:
+                        # 使用异步生成器产生数据块
+                        async for chunk in response.content:
+                            yield chunk
+                    else:
+                        print("Failed to retrieve data:", response.status)
+                        yield b''  # 返回空字节，表示错误或无数据
 
+    
     def __del__(self):
         for proc in self.recv_controller_procs:
             proc.terminate()
